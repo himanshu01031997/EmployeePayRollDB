d9b8d03c93834f117bdc4fb7c8ea30e2872710dc
himanshu01031997 <hs010397@gmail.com>
2022-11-10 22:19:13 +0530

    add:table

--- employeePayRollDatabaseDay31.sql
+++ employeePayRollDatabaseDay31.sql
@@ -1,2 +1,13 @@
 --------------------UC1---------------
 Create database EmployeePAYRolldatabase
+
+
+------------------------UC2-----------------
+create table employeepayroll
+(
+columnid int ,
+name varchar(200),
+salary int ,
+startdate date,
+)
+select * from employeepayroll
