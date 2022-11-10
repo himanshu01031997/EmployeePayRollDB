54d3dfaaf11f0c01aac141a3087567f2b9a30811
himanshu01031997 <hs010397@gmail.com>
2022-11-10 22:34:07 +0530

    data inserted into table

--- employeePayRollDatabaseDay31.sql
+++ employeePayRollDatabaseDay31.sql
@@ -1,13 +1,16 @@
 --------------------UC1---------------
 Create database EmployeePAYRolldatabase
 
-
 ------------------------UC2-----------------
 create table employeepayroll
 (
-columnid int ,
+columnid int identity(1,1) primary key,
 name varchar(200),
 salary int ,
 startdate date,
 )
 select * from employeepayroll
+--------------------UC3-----------------
+insert into employeepayroll values('himanshu',20000,'2002-06-20')
+insert into employeepayroll values('chota',30000,'2002-03-23')
+insert into employeepayroll values('mini',10000,'2002-05-20')
